a142b58cc7eb20709ea81c69079af6e522cef1bb
SivaKrishnaBatta <battasivakrishna@gmail.com>
2026-06-09 12:21:22 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from './observable';
+import { CommonModule } from '@angular/common';
+import{FormsModule,NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('Practice');
+export class App implements OnInit {
+
+  users: any[] = [];
+  constructor( private observable: Observable) {}
+
+  ngOnInit(): void {
+    this.observable.getData().subscribe((data:any)=>{
+      this.users=data;
+      console.log(this.users);
+    })
+
+  }
+  onLogin(form:NgForm){
+    console.log(form.value);
+    const email=form.value.email;
+    const password=form.value.password;
+    console.log("email :", email);
+    console.log("password :", password);
+    form.reset();
+  }
 }
